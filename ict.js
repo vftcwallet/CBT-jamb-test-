f29872ee28ca792b7799b36d6ce04e862e0cb549
vftcwallet <vftcwallet@gmail.com>
2026-03-07 05:03:40 +0100

    Add files via upload

---
+++ ict.js
@@ -0,0 +1,56 @@
+const ict = [
+{
+  question: "CPU stands for:",
+  options: ["Central Process Unit", "Central Processing Unit", "Computer Personal Unit", "Control Process Unit"],
+  answer: 1
+},
+{
+  question: "RAM is:",
+  options: ["Permanent memory", "Temporary memory", "External storage", "Firmware"],
+  answer: 1
+},
+{
+  question: "HTML is used for:",
+  options: ["Programming", "Database", "Web structure", "Networking"],
+  answer: 2
+},
+{
+  question: "who created this app",
+  options: ["AbdulRahman", "AbduLsamad", "isah", "abu"],
+  answer: 1
+},
+{
+  question: "IP address identifies:",
+  options: ["User", "Computer on network", "Website name", "Browser"],
+  answer: 1
+},
+{
+  question: "Operating system example:",
+  options: ["Windows", "CPU", "RAM", "Mouse"],
+  answer: 0
+},
+{
+  question: "Binary system uses:",
+  options: ["0-9", "0 and 1", "1-10", "A-F"],
+  answer: 1
+},
+{
+  question: "ROM is:",
+  options: ["Editable memory", "Read-only memory", "Temporary memory", "Cache"],
+  answer: 1
+},
+{
+  question: "LAN means:",
+  options: ["Local Area Network", "Large Area Network", "Line Access Node", "Linked Access Network"],
+  answer: 0
+},
+{
+  question: "Compiler converts:",
+  options: ["Machine to English", "High-level to machine code", "Binary to text", "Text to binary"],
+  answer: 1
+},
+{
+  question: "Database stores:",
+  options: ["Music", "Videos", "Structured data", "Images only"],
+  answer: 2
+}];
